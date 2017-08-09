bffd9b830d546f539e1ca5583718c96e6fcb56f5
santhiyasivakumar <30791898+santhiyasivakumar@users.noreply.github.com>
2017-08-08 21:49:06 -0700

    Create 11.c

---
+++ 11.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h.
+int main()
+{
+int base,exponent,counter result=1;
+printf("enter base&exponent\n");
+scanf("%d%d",&base,&exponent;counter++)
+{
+result=result*base;
+}
+printf("%d%d=%d",base,exponent,result;
+getch();
+return 0;
+}
